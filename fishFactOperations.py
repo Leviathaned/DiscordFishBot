fa3e35c056f5f4c51d218e8a6f16d0cda5e5daf9
Leviathonk <91150620+Leviathonk@users.noreply.github.com>
2023-02-14 18:27:36 -0500

    Changes to fishFactOperations.py for stability.
    This change makes fishFactOperations.py use __file__ from os so that the raspberry pi can access the filepath properly. This change also adds in a buffer layer for all tables, so that pandas can access the tables without trying to add NaN values into the dataframe unnecessarily.

--- fishFactOperations.py
+++ fishFactOperations.py
@@ -1,41 +1,47 @@
-import csv
+import os
 import random
 import pandas
 
+
 def getFishFactCount(serverName):
-    df = pandas.read_json("fishFacts.json")
-    fishFacts = df[serverName]
+    basePath = os.path.dirname(os.path.abspath(__file__))
+    df = pandas.read_json(basePath + r'\fishFacts.json')
+    fishFacts = df[serverName][0]
+    print(fishFacts)
     return len(fishFacts)
 
 
 def grabFishFact(serverName):
-    df = pandas.read_json("fishFacts.json")
-    randomFactIndex = random.randint(0, len(df[serverName]) - 1)
-    print(df[serverName])
-    return str(randomFactIndex + 1) + ') ' + df[serverName][randomFactIndex]
+    basePath = os.path.dirname(os.path.abspath(__file__))
+    df = pandas.read_json(basePath + r'\fishFacts.json')
+    randomFactIndex = random.randint(0, len(df[serverName][0]) - 1)
+    return str(randomFactIndex + 1) + ') ' + df[serverName][0][str(randomFactIndex)]
 
 
 def grabSpecificFishFact(serverName, i):
-    df = pandas.read_json("fishFacts.json")
-    fishFact = df[serverName][i - 1]
+    basePath = os.path.dirname(os.path.abspath(__file__))
+    df = pandas.read_json(basePath + r'\fishFacts.json')
+    fishFact = df[serverName][0][str(i - 1)]
     return str(i) + ') ' + fishFact
 
 
 def addFishFact(serverName, fishFact):
-    df = pandas.read_json("fishFacts.json")
+    basePath = os.path.dirname(os.path.abspath(__file__))
+    df = pandas.read_json(basePath + r'\fishFacts.json')
     factCount = getFishFactCount(serverName)
-    df[serverName][str(factCount)] = fishFact
+    df[serverName][0][str(factCount)] = fishFact
     df.to_json(r'fishFacts.json')
 
 
 def removeFishFact(serverName, index):
-    df = pandas.read_json("fishFacts.json")
-    factCount = getFishFactCount(serverName)
+
+    basePath = os.path.dirname(os.path.abspath(__file__))
+    df = pandas.read_json(basePath + r'\fishFacts.json')
 
     # Make this actually signal that the process has failed, and have the bot notify the user of error
-    if len(df[serverName]) > index:
+    if len(df[serverName][0]) > index:
         return
 
-    del df[serverName][index - 1]
+    del df[serverName][0][str(index - 1)]
     df.to_json(r'fishFacts.json')
     return
--- fishFactTests.py
+++ fishFactTests.py
@@ -4,19 +4,17 @@ import unittest
 class TestFishFacts(unittest.TestCase):
 
     def testCountFishFacts(self):
-        self.assertEqual(2, fishFactOperations.getFishFactCount("testing"), "There is an error in getFishFactCount!")
+        self.assertEqual(1, fishFactOperations.getFishFactCount("testing"), "There is an error in getFishFactCount!")
 
     def testGrabFishFact(self):
         fishFact = fishFactOperations.grabFishFact("testing")
-        print(fishFact)
         self.assertEqual(fishFact, "1) This is testing fish fact #1!")
 
     def testGrabSpecificFishFact(self):
         fishFact = fishFactOperations.grabSpecificFishFact("testing2", 2)
-        print(fishFact)
         self.assertEqual(fishFact, "2) This is testing fish fact #2 from the second test server!")
 
     def testAddAndRemove(self):
-        fishFactOperations.addFishFact("testing", "Hello, and welcome to the next sample fish fact!")
-        fishFactOperations.removeFishFact("testing", 2)
-        self.assertEqual(2, fishFactOperations.getFishFactCount("testing"), "There is an error in getFishFactCount!")
+        fishFactOperations.addFishFact("testing2", "Hello, and welcome to the next sample fish fact!")
+        fishFactOperations.removeFishFact("testing2", 3)
+        self.assertEqual(2, fishFactOperations.getFishFactCount("testing2"), "There is an error in getFishFactCount!")
