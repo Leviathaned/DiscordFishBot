4e2e60a2b907eed9a445cc66d9e8304e87825e62
Levi Shelley <levi.shelleye@gmail.com>
2023-02-14 22:43:32 -0500

    Changes fishBotScript & Operations to work with changes

--- fishBotScript.py
+++ fishBotScript.py
@@ -31,8 +31,10 @@ async def on_message(message):
 
     if message.content.startswith('+'):
         userMessage = message.content.split(" ", 1)
+    else:   
+        return
 
-    if userMessage[0] == '+hello!':
+    if userMessage[0] == '+hello':
         await message.channel.send("Hello World!")
 
     if userMessage[0] == '+help':
@@ -46,40 +48,43 @@ Hello! I am the fishing bot! Here are the commands I currently support:
 +help: Get info about my commands!
 """)
 
+    if userMessage[0] == '+getServerName':
+        print(message.guild.name)
+
     if userMessage[0] == '+getFishFact':
         if len(userMessage) == 1:
             await message.channel.send(fishFactOperations.grabFishFact(message.guild.name))
         else:
             try:
-                await message.channel.send(fishFactOperations.grabSpecificFishFact(message.guild.name, int(userMessage[1]) - 1))
-            finally:
+                await message.channel.send(fishFactOperations.grabSpecificFishFact(message.guild.name, int(userMessage[1])))
+            except:
                 traceback.print_exc()
                 await message.channel.send(
                     f'That number is either too high, or not a number at all! For reference, I currently have {fishFactOperations.getFishFactCount(message.guild.name)} fish facts!')
 
-        if userMessage[0] == '+addFishFact':
-            try:
-                fishFactOperations.addFishFact(message.guild.name, userMessage[1])
-                await message.channel.send(
-                    f"Fish fact '{fishFactOperations.getFishFactCount(message.guild.name)}) {userMessage[1]}' successfully added!")
-            finally:
-                await message.channel.send(
-                    "There was an issue adding the fish fact. Did you properly space the fact out, and keep it right after the command?")
-
-        if userMessage[0] == '+removeFishFact':
+    if userMessage[0] == '+addFishFact':
+        try:
+            fishFactOperations.addFishFact(message.guild.name, userMessage[1])
             await message.channel.send(
-                f"Are you sure you want to remove the fish fact {fishFactOperations.grabSpecificFishFact(message.guild.name ,int(userMessage[1]) - 1)} permanently? Please type 'Yes' to confirm, and anything else to cancel.")
-            channel = message.channel
+                f"Fish fact '{fishFactOperations.getFishFactCount(message.guild.name)}) {userMessage[1]}' successfully added!")
+        except:
+            await message.channel.send(
+                "There was an issue adding the fish fact. Did you properly space the fact out, and keep it right after the command?")
 
-            def check(m):
-                return m.channel == channel and m.author == message.author
+    if userMessage[0] == '+removeFishFact':
+        await message.channel.send(
+            f"Are you sure you want to remove the fish fact {fishFactOperations.grabSpecificFishFact(message.guild.name ,int(userMessage[1]))} permanently? Please type 'Yes' to confirm, and anything else to cancel.")
+        channel = message.channel
 
-            msg = await client.wait_for("message", check=check)
-            if msg.content == "Yes":
-                fishFactOperations.removeFishFact(message.guild.name, int(userMessage[1]) - 1)
-                await message.channel.send("Ok, the fish fact has been deleted.")
-            else:
-                await message.channel.send("Ok, the delete command has been canceled.")
+        def check(m):
+            return m.channel == channel and m.author == message.author
+
+        msg = await client.wait_for("message", check=check)
+        if msg.content == "Yes":
+            fishFactOperations.removeFishFact(message.guild.name, int(userMessage[1]))
+            await message.channel.send("Ok, the fish fact has been deleted.")
+        else:
+            await message.channel.send("Ok, the delete command has been canceled.")
 
 
 client.run(TOKEN)
--- fishFactOperations.py
+++ fishFactOperations.py
@@ -5,29 +5,28 @@ import pandas
 
 def getFishFactCount(serverName):
     basePath = os.path.dirname(os.path.abspath(__file__))
-    df = pandas.read_json(basePath + r'\fishFacts.json')
+    df = pandas.read_json(basePath + r'/fishFacts.json')
     fishFacts = df[serverName][0]
-    print(fishFacts)
     return len(fishFacts)
 
 
 def grabFishFact(serverName):
     basePath = os.path.dirname(os.path.abspath(__file__))
-    df = pandas.read_json(basePath + r'\fishFacts.json')
+    df = pandas.read_json(os.path.join(basePath, 'fishFacts.json'))
     randomFactIndex = random.randint(0, len(df[serverName][0]) - 1)
     return str(randomFactIndex + 1) + ') ' + df[serverName][0][str(randomFactIndex)]
 
 
 def grabSpecificFishFact(serverName, i):
     basePath = os.path.dirname(os.path.abspath(__file__))
-    df = pandas.read_json(basePath + r'\fishFacts.json')
+    df = pandas.read_json(basePath + r'/fishFacts.json')
     fishFact = df[serverName][0][str(i - 1)]
     return str(i) + ') ' + fishFact
 
 
 def addFishFact(serverName, fishFact):
     basePath = os.path.dirname(os.path.abspath(__file__))
-    df = pandas.read_json(basePath + r'\fishFacts.json')
+    df = pandas.read_json(basePath + r'/fishFacts.json')
     factCount = getFishFactCount(serverName)
     df[serverName][0][str(factCount)] = fishFact
     df.to_json(r'fishFacts.json')
@@ -36,7 +35,7 @@ def addFishFact(serverName, fishFact):
 def removeFishFact(serverName, index):
 
     basePath = os.path.dirname(os.path.abspath(__file__))
-    df = pandas.read_json(basePath + r'\fishFacts.json')
+    df = pandas.read_json(basePath + r'/fishFacts.json')
 
     # Make this actually signal that the process has failed, and have the bot notify the user of error
     if len(df[serverName][0]) > index:
